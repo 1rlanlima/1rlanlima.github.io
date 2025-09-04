4a722f7dffb45255eb833dd786ceebc33ac42d77
Irlan Lima <irlan13lima@gmail.com>
2025-09-04 19:16:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,12 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const loadingIndicator = document.querySelector('.loading-indicator');
 
     startButton.addEventListener('click', () => {
-        // Ativa o indicador de carregamento
         loadingIndicator.classList.add('active');
 
-        // Simula um tempo de carregamento antes de ir para a próxima página
         setTimeout(() => {
-            window.location.href = 'game.html'; // Redireciona para a página do jogo
-        }, 1500); // 1.5 segundos de "carregamento"
+            window.location.href = 'game.html';
+        }, 1500);
     });
+
 });
